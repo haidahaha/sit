6a989ab48a447b358ebbe679d482e82165a85f5e
Samuel Gyger <samuel@gyger.at>
2014-10-11 17:23:21 +0200

    Revert "Changed to local SSL Store" Set Debug Mode to no SSL Check This reverts commit 3946eeb8be006033fc4ea00996ac499323eff4a0.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ENV['SSL_CERT_FILE'] = File.expand_path('../cacert.pem', __FILE__)
