602a5a51b357348bfb2b742baf879c43bc6a899a
Samuel Gyger <samuel@gyger.at>
2014-10-11 12:39:36 +0200

    Added API to get Subjects of Webpage. Added Demo.

--- app/controllers/sharings_controller.rb
+++ app/controllers/sharings_controller.rb
@@ -21,5 +21,13 @@ class SharingsController < ApplicationController
          }
      ]
     @web_url = "http://www.google.com"
+    @subjects = get_subjects(@web_url)
+  end
+  
+  protected
+  def get_subjects (url)
+    hashtags = Aylien.get_hashtags(url)
+    #Remove # from Hashtag and convert CamelCase to Space separated Nouns.
+    hashtags.map {|hashtag| hashtag[1..-1].gsub(/([a-z])([A-Z])/, '\1 \2')}
   end
 end
