3946eeb8be006033fc4ea00996ac499323eff4a0
Samuel Gyger <samuel@gyger.at>
2014-10-11 17:18:44 +0200

    Changed to local SSL Store

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV['SSL_CERT_FILE'] = File.expand_path('../cacert.pem', __FILE__)
